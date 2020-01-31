20bc5cda30b65a86d184bb5de559047d6e9435c5
Celia Sherry <sherry.c@husky.neu.edu>
2020-01-30 18:04:48 -0800

    create offset dict

--- HW2/Assignment2/main.py
+++ HW2/Assignment2/main.py
@@ -1,8 +1,20 @@
 from bs4 import BeautifulSoup
 import regex
 import re
-import os
 from nltk.stem import PorterStemmer
+from collections import defaultdict
+
+# Store term frequency and term position
+class Term:
+    def __init__(self, tf, position):
+        self.tf = tf
+        self.position = position
+
+    def get_term_frequency(self):
+        return self.tf
+
+    def get_position(self):
+        return self.position
 
 stemmer = PorterStemmer()
 
@@ -82,8 +94,38 @@ def tokenize(text):
             token_list.append([token, i])
     return token_list
 
+
+# Construct dictionary of terms that contain Terms- hold term frequency and position
+def construct_offset_dict(given_tokens):
+    offset_dict = defaultdict(lambda: defaultdict(Term))
+    stopwords = get_stopwords()
+    for token in given_tokens:
+        doc_id = token[2]
+        if token[0] not in stopwords:
+            if token[0] in offset_dict:
+                if doc_id in offset_dict[token[0]]:
+                    offset_dict[token[0]][doc_id].tf = offset_dict[token[0]][doc_id].get_term_frequency() + 1
+                    offset_dict[token[0]][doc_id].position.append(token[1])
+                else:
+                    offset_dict[token[0]][doc_id] = Term(1, [token[1]])
+            else:
+                doc_dict = defaultdict(Term)
+                doc_dict[doc_id] = Term(1, [token[1]])
+                offset_dict[token[0]] = doc_dict
+    return offset_dict
+
+
+def load_catalog(offset_dict, file_name, inv_file_num, catalog_file = None):
+    f = '%s%s.txt' %(file_name, inv_file_num)
+
+
+
 def create_index(tokens, flag, inv_file):
-    doc_id = tokens[0][2]
+    # Make dictionary of terms- offsets dictionary
+    offset_dict = construct_offset_dict(tokens)
+    # Create inverted file
+    inv_file = load_catalog(offset_dict, "inverted_file", inv_file)
+    return inv_file
 
 
 def get_tokens():
@@ -120,7 +162,6 @@ def get_tokens():
         # Add document id to each token so it is (term, position, document_id)
         for token in token_positions:
             token.append(doc_id)
-
         # Add position tokens to list of all tokens
         tokens += token_positions
 
@@ -129,10 +170,15 @@ def get_tokens():
             inv_file = create_index(tokens, flag, inv_file)
             tokens = []
             flag = 0
-    if num_docs < 1000
+    if num_docs < 1000:
         inv_file = create_index(tokens, flag, inv_file)
 
 
 
 #print(get_document_length(get_text(get_tokens()[0])))
-print(tokenize("this is a test. movies 1895 10.20 1,000 this"))
+#print(tokenize("this is a test. movies 1895 10.20 1,000 this"))
+#print(get_tokens())
+tokens = get_tokens()
+print(tokens)
+#d = construct_offset_dict(tokens)
+#print(d['people'])
